15f7870947769a73606d9bbd6f812f5883b7dbc6
tilde <tilde@tildes-MacBook-Pro.local>
2017-10-21 20:42:55 +0800

    实现销售和营收的报表模块

---
+++ scripts/xiaobu_report_v0.2.py
@@ -0,0 +1,440 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+'''
+xiao bu data analysis
+'''
+import os
+import sys
+import shutil
+import csv
+import re
+import datetime
+import time
+# from string import Template
+
+class DataParser(object):
+    '''
+     Data Parser
+    '''
+    MAX_COLUMNS = 58
+    def __init__(self, raw_data):
+        self.rawData = raw_data
+        self.__data = []
+        self.__header = []
+
+    def __parse_data(self):
+        '''  parse data from csv file  '''
+        assert (self.rawData.__len__() > 0)
+        for row in self.rawData:
+            if row.__len__() == DataParser.MAX_COLUMNS:
+                if self.__header.__len__() == 0:
+                    self.__header = row
+                else:
+                    self.__data.append(row)
+
+    def index_of_key(self, key):
+        ''' find index of the specified key'''
+        header = self.get_header()
+        return header.index(key) if header.__contains__(key) else -1
+
+    def filte_data(self, key, included_values):
+        ''' filter data by key which value is  in the included_values ''' 
+        index = self.index_of_key(key)
+        assert index >= 0
+        raw_data = self.get_data()
+        filted_data = filter(lambda d: included_values.__contains__(d[index]), raw_data)
+        filted_data.insert(0, self.get_header())
+        return filted_data
+
+    def get_header(self):
+        '''
+            getter: header
+        '''
+        if self.__header.__len__() == 0:
+            self.__parse_data()
+        return self.__header
+
+    def get_data(self):
+        ''' getter: data '''
+        if self.__data.__len__() == 0:
+            self.__parse_data()
+        return self.__data
+    
+    def get_data_from_column_title(self, title, input_data):
+        ''' get column data with title name '''
+        index = self.index_of_key(title)
+        assert index >= 0
+        return map(lambda row: row[index], input_data)
+
+class FruitLabel(object):
+    ''' the fruit printing label object '''
+    MAX_PRINTING_FRUIT = 5
+
+    class Label(object):
+
+        def __init__(self, tel, user, address, message):
+            self.user = user
+            self.tel = tel
+            self.address = address
+            self.message = message
+            self.fruits = [] #{'name', 'amount', 'price'}  
+        
+    TITLES = ("序号", "姓名", "电话", "地址", "水果-1数量","水果-1", "水果-2数量", "水果-2", "水果-3数量", "水果-3", "水果-4数量", "水果-4", "水果-5数量", "水果-5", "总金额", "留言")
+    
+    def __init__(self, fruit_data):
+        self.__labels = []
+        self.fruit_data = fruit_data
+
+    def __parser_data(self):
+        data_parser = DataParser(self.fruit_data)
+        label_data = data_parser.get_data()
+        data_of_users = data_parser.get_data_from_column_title(XiaoBuReport.TITLE_USER_NAME, label_data)
+        data_of_numbers = data_parser.get_data_from_column_title(XiaoBuReport.TITLE_USER_PHONE, label_data)
+        data_of_addresss = data_parser.get_data_from_column_title(XiaoBuReport.TITLE_COMMUNITY_POINT, label_data)
+        data_of_messages = data_parser.get_data_from_column_title(XiaoBuReport.TITLE_ORDER_MESSAGE, label_data)
+        data_of_fruit_names = data_parser.get_data_from_column_title(XiaoBuReport.TITLE_PRODUCT_NAME, label_data)
+        data_of_fruit_amounts = data_parser.get_data_from_column_title(XiaoBuReport.TITLE_PRODUCT_AMOUNT, label_data)
+        data_of_fruit_prices = data_parser.get_data_from_column_title(XiaoBuReport.TITLE_PRODUCT_PRICE, label_data)
+
+        sorted_data = {}
+        for index, tel_number in enumerate(data_of_numbers):
+
+            if not sorted_data.has_key(tel_number):
+                fruit_label = FruitLabel.Label(tel_number, data_of_users[index], data_of_addresss[index], data_of_messages[index])
+                sorted_data[tel_number] = fruit_label
+            
+            sorted_data[tel_number].fruits.append({'name': data_of_fruit_names[index], 'amount': data_of_fruit_amounts[index], 'price': data_of_fruit_prices[index]})
+        
+        for fruit_label in sorted_data.values():
+            if not fruit_label.fruits.__len__() > FruitLabel.MAX_PRINTING_FRUIT:
+                self.__labels.append(fruit_label)
+            else:
+                sub_fruit_label = FruitLabel.Label(fruit_label.tel, fruit_label.user, fruit_label.address, fruit_label.message)
+                sub_fruit_label.fruits = fruit_label.fruits[FruitLabel.MAX_PRINTING_FRUIT:]
+                fruit_label.fruits = fruit_label.fruits[:FruitLabel.MAX_PRINTING_FRUIT]
+                self.__labels.append(fruit_label)
+                self.__labels.append(sub_fruit_label)
+
+    def get_datas(self):
+        rows = []
+        if self.__labels.__len__() == 0:
+            self.__parser_data()
+
+        for idx, label in enumerate(self.__labels):
+            fruit_infos = []
+            total_prices = 0
+            max_fruit_categories = FruitLabel.MAX_PRINTING_FRUIT
+            for fruit in label.fruits:
+                fruit_amount = fruit['amount']
+                fruit_price = fruit['price'] 
+                fruit_name = fruit['name']
+                fruit_infos.append(fruit_amount)
+                fruit_infos.append("x %s" % (fruit_name))
+                total_prices += (int(fruit_amount) * float(fruit_price))
+                max_fruit_categories -= 1
+
+            #水果不够Max时用空字符填充。
+            while max_fruit_categories > 0:
+                fruit_infos.append("")
+                fruit_infos.append("")
+                max_fruit_categories -= 1
+            fruit_infos.append(total_prices)
+            fruit_infos.append(label.message)
+
+            row = [idx+1, label.user, label.tel, label.address]
+            row.extend(fruit_infos)
+
+            rows.append(row)
+
+        return rows
+
+class XiaoBuReport(object):
+    ''' Xiao bu report generator'''
+
+    TITLE_COMMUNITY_POINT = '自提网点'
+    TITLE_ORDER_STATUS = '订单状态'
+    TITLE_PRODUCT_NAME = '宝贝标题'
+    TITLE_PRODUCT_AMOUNT = '宝贝总数量'
+    TITLE_PRODUCT_PRICE = '商品价格'
+    TITLE_USER_PAYMENT_AMOUNT = '买家实际支付商品金额'
+    TITLE_USER_NAME = '预约人'
+    TITLE_USER_PHONE = '预约电话'
+    TITLE_ORDER_MESSAGE = '订单留言'
+    TITLE_ORDER_CREATED_DATE= '订单创建时间'
+    ORDER_STATUS_OF_DELIVERED = '交易完成'
+    ORDER_STATUS_OF_UNDELIVERED = '等待商家发货'
+
+    DRI_TEL_MAP = {
+        '13689700006': '李海', 
+        '15104030086': '黄旭玲', 
+        '18842410372': '何志强',
+        '15041205708': '冯兴龙',
+        '13810992951': '刘鹤',
+        '15304022399': '刘铁磊',
+        '15940087939': '宋利燕',
+        '13897932270': '王亚平',
+        '13897926945': '陈志扬',
+        '13322496960': '朱莹',
+        '15998336739': '金吉营',
+        '15004038283': '隋明发',
+        '18640520539': '王鹭',
+        '18040036608': '冮星光',
+        '18624034797': '邓铁梅',
+        }
+
+    def __init__(self, report_folder):
+        self.__path = os.path.join(os.getcwd(),report_folder)
+
+    def get_report_path(self):
+        ''' get report folder path '''
+        if not os.path.exists(self.__path):
+            os.makedirs(self.__path)
+        return self.__path
+    
+    def report_order_details_by_community(self, community_name, detail_data, csv_file_name):
+        ''' report by community '''
+        community_path = os.path.join(self.__path, community_name)
+        detail_csv_path = os.path.join(community_path, csv_file_name)
+
+        if  os.path.exists(community_path):
+            shutil.rmtree(community_path)
+        os.makedirs(community_path)
+        Utils.write_data_to_csv(detail_data, detail_csv_path)
+
+    def __fruit_summary_data(self, fruit_data):
+        data_parser = DataParser(fruit_data)
+        datas = data_parser.get_data()
+        fruit_names = data_parser.get_data_from_column_title(XiaoBuReport.TITLE_PRODUCT_NAME, datas)
+        fruit_amounts = data_parser.get_data_from_column_title(XiaoBuReport.TITLE_PRODUCT_AMOUNT, datas)
+        fruit_prices = data_parser.get_data_from_column_title(XiaoBuReport.TITLE_PRODUCT_PRICE, datas)
+        assert fruit_names.__len__() == fruit_amounts.__len__()
+        sorted_datas = {}
+        for index, fruit in enumerate(fruit_names):
+            if not sorted_datas.has_key(fruit):
+                sorted_datas[fruit] = {'amount': int(fruit_amounts[index]), 'price': fruit_prices[index]}
+            else:
+                sorted_datas[fruit] = {'amount': sorted_datas[fruit]['amount'] + int(fruit_amounts[index]), 'price': fruit_prices[index]}
+
+        row_datas = []
+        all_keys = sorted_datas.keys()
+        all_amounts = [sorted_datas[k]['amount'] for k in all_keys]
+        all_prices = [sorted_datas[k]['price'] for k in all_keys]
+        all_supplier_values = []
+        all_total_prices = []
+        for idx, x in enumerate(all_keys):
+            m = re.match('^.*\/(\d+).*$', x)
+            multiple = m.group(1) if m else 1
+            all_supplier_values.append(int(multiple) * int(all_amounts[idx]))
+            all_total_prices.append(float(all_prices[idx]) * int(all_amounts[idx]))
+        all_keys.insert(0, '明细')
+        all_amounts.insert(0, '系统下单数量')
+        all_supplier_values.insert(0, '供应商数量')
+        all_prices.insert(0, '水果单价') 
+        all_total_prices.insert(0, '各水果销售总额数')
+
+        row_datas.append(all_keys)
+        row_datas.append(all_amounts) 
+        row_datas.append(all_supplier_values) 
+        row_datas.append(all_prices)
+        row_datas.append(all_total_prices)
+
+        return row_datas
+    
+    def __community_analysis_data(self, community_data):
+        ''' 按小区分析营收情况 '''
+        community_data_parser = DataParser(community_data)  
+        datas = community_data_parser.get_data()
+        index_of_community = community_data_parser.index_of_key(XiaoBuReport.TITLE_COMMUNITY_POINT)
+        index_of_tel = community_data_parser.index_of_key(XiaoBuReport.TITLE_USER_PHONE)
+        index_of_payment_amount = community_data_parser.index_of_key(XiaoBuReport.TITLE_USER_PAYMENT_AMOUNT)
+        index_of_order_create_date = community_data_parser.index_of_key(XiaoBuReport.TITLE_ORDER_CREATED_DATE)
+        analysis_entries = {}
+
+        xiaobo_turnover = 0
+
+        for row in datas:
+            community_name = row[index_of_community]
+            user_payment_amount = float(row[index_of_payment_amount])
+            timestamp = time.mktime(time.strptime(row[index_of_order_create_date], '%Y-%m-%d %H:%M:%S'))
+            user_tel = row[index_of_tel]
+            m = re.match('.*\s+(\d+)\s*$', community_name)
+            tel = m.group(1) if m else "未知电话"
+            user = XiaoBuReport.DRI_TEL_MAP[tel] if XiaoBuReport.DRI_TEL_MAP.has_key(tel) else "未知负责人"
+
+            if not analysis_entries.has_key(tel):
+                entry = {
+                'tel': tel, 
+                'user': user, 
+                'community': community_name, 
+                'orders_of_count': 1, 
+                'community_turnover': user_payment_amount, 
+                'start_timestamp': timestamp,
+                'end_timestamp': timestamp,
+                'dri_payment': 0.0
+                }
+                analysis_entries[tel] = entry
+            
+            analysis_entries[tel]['orders_of_count'] += 1
+            analysis_entries[tel]['community_turnover'] += user_payment_amount
+            analysis_entries[tel]['start_timestamp'] = timestamp if analysis_entries[tel]['start_timestamp'] > timestamp else analysis_entries[tel]['start_timestamp']
+            analysis_entries[tel]['end_timestamp'] = timestamp if analysis_entries[tel]['end_timestamp'] < timestamp else analysis_entries[tel]['end_timestamp']
+
+            if tel == user_tel:
+                analysis_entries[tel]['dri_payment'] += user_payment_amount
+
+            xiaobo_turnover += user_payment_amount
+
+        analysis_entries['xiaobu_turnover'] = xiaobo_turnover
+        return analysis_entries
+
+    def report_of_financial(self,financial_data, financial_file_path):
+        ''' 财务报表 '''
+        fruit_datas = self.__fruit_summary_data(financial_data)
+        Utils.write_data_to_xls(fruit_datas, financial_file_path)
+
+    def report_of_purchase(self, purchase_data, purchase_file_path):
+        '''采购报表 '''
+        fruit_datas = self.__fruit_summary_data(purchase_data)
+        Utils.write_data_to_xls(fruit_datas[0:3], purchase_file_path)
+
+    def report_printing_information(self, printing_data, printing_file_path):
+        ''' 打印信息的报表 '''
+        fruit_lable = FruitLabel(printing_data)
+        file_datas = fruit_lable.get_datas()
+        file_datas.insert(0, FruitLabel.TITLES)
+        Utils.write_data_to_xls(file_datas, printing_file_path)
+
+    def report_of_sales(self, sales_data, sales_file_path):
+        ''' 销售报表 '''
+        sales_report_title =('序号', '小区名称', '小区负责人', '电话', '小布总营业额(元)', '小区营业额(元)', '负责人购买(元)', '营业额占比', '总利润', '小区负责人营收(元)', '统计时间段', '备注')
+        sales_entries = self.__community_analysis_data(sales_data)
+        if not sales_entries['xiaobu_turnover'] > 0: 
+            print("警告：没有找到销售数据，销售报表生成失败!")
+            return
+
+        xiaobu_turnover = sales_entries.pop('xiaobu_turnover')
+
+        idx = 1
+        report_data = [sales_report_title]
+        for tel, communities_data in sales_entries.iteritems():
+            community = communities_data['community']
+            user = communities_data['user']
+            tel = communities_data['tel']
+            community_turnover = communities_data['community_turnover']
+            dri_payment = communities_data['dri_payment']
+            community_turnover_rate = "%.2f %%" % ((community_turnover / xiaobu_turnover) * 100)
+            start_date = time.strftime('%Y-%m-%d', time.localtime(communities_data['start_timestamp']))
+            end_date = time.strftime('%Y-%m-%d', time.localtime(communities_data['end_timestamp']))
+            periods = "%s -%s" % (start_date, end_date)
+            gross_profit = ""
+            dri_revenue = ""
+            mark = ""
+            row_data = (idx, community, user, tel, xiaobu_turnover, community_turnover, dri_payment, community_turnover_rate, gross_profit,dri_revenue, periods, mark)
+            report_data.append(row_data)
+            idx += 1
+        Utils.write_data_to_xls(report_data, sales_file_path)
+    
+    def report_of_revenue(self, revenue_data, revenue_file_path):
+        ''' 营收报表 '''
+        revenue_report_title =('序号', '小区名称', '小区负责人', '电话', '小区营业额(元)', '负责人购买(元)', '订单总数', '时间', '备注')
+        revenue_entries = self.__community_analysis_data(revenue_data)
+        if not revenue_entries['xiaobu_turnover'] > 0: 
+            print("警告：没有找到营收数据，营收报表生成失败!")
+            return
+
+        xiaobu_turnover = revenue_entries.pop('xiaobu_turnover')
+
+        idx = 1
+        report_data = [revenue_report_title]
+        for tel, communities_data in revenue_entries.iteritems():
+            community = communities_data['community']
+            user = communities_data['user']
+            tel = communities_data['tel']
+            community_turnover = communities_data['community_turnover']
+            dri_payment = communities_data['dri_payment']
+            report_date = time.strftime('%Y-%m-%d', time.localtime(communities_data['end_timestamp']))
+            count_of_orders = communities_data['orders_of_count']
+            mark = ""
+            row_data = (idx, community, user, tel, community_turnover, dri_payment, count_of_orders, report_date, mark)
+            report_data.append(row_data)
+            idx += 1
+        Utils.write_data_to_xls(report_data, revenue_file_path)   
+        
+
+class Utils(object):
+    ''' utils '''
+
+    @staticmethod
+    def csv_data_from_file(csv_file_path):
+        ''' read data from csv file '''
+        csv_data = []
+        with open (csv_file_path, 'rb') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            for row in spamreader:
+                csv_data.append([d.decode('gb18030').encode('utf8') for d in row])
+        return csv_data
+
+    @staticmethod
+    def write_data_to_csv(datas, csv_file_path):
+        ''' write data to csv file '''
+        with open(csv_file_path, 'wb') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            for row in datas:
+                spamwriter.writerow(row)
+    
+    @staticmethod
+    def write_data_to_xls(datas, xls_file_path):
+        ''' write data to xls file '''
+        if datas.__len__() == 0: 
+            return
+        xls_start_template = '''<html>
+        <head> <meta charset="UTF-8"> </head>
+         <table border="1">
+        '''
+        xls_end_template = '''</table>
+         </html>'''
+        with open(xls_file_path, 'wb') as xlsfile:
+            xlsfile.write(xls_start_template)
+            xls_header_data = datas[0]
+            xlsfile.write("<thead>\n<th>" + "</th><th>".join(xls_header_data) + "</th>\n</thead> \n")
+            xls_body_data = datas[1:]
+
+            if xls_body_data.__len__() > 0:
+                xlsfile.write("<tbody>\n")
+                for row in xls_body_data:
+                    xlsfile.write("<tr><td>" + "</td><td>".join([str(d) for d in row]) + "</td></tr>\n")
+                xlsfile.write("</tbody>\n")
+            xlsfile.write(xls_end_template)
+
+if  __name__ == '__main__':
+    if sys.argv.__len__() != 2:
+        print("usage: <script> <input_csv_file>")
+        sys.exit(-1)
+
+    CSV_DATA_PATH = sys.argv[1]
+    raw_data = Utils.csv_data_from_file(CSV_DATA_PATH)
+
+    xbr = XiaoBuReport('小布报表')
+    report_path = xbr.get_report_path()
+    print("报表路径: " + xbr.get_report_path())
+
+    data_parser = DataParser(raw_data)
+    undelivered_orders = data_parser.filte_data(XiaoBuReport.TITLE_ORDER_STATUS, (XiaoBuReport.ORDER_STATUS_OF_UNDELIVERED))
+    delivered_orders = data_parser.filte_data(XiaoBuReport.TITLE_ORDER_STATUS, (XiaoBuReport.ORDER_STATUS_OF_DELIVERED))
+
+    xbr.report_of_purchase(undelivered_orders, os.path.join(report_path, datetime.datetime.now().strftime('采购需求_%Y-%m-%d.xls'))) 
+    xbr.report_of_financial(undelivered_orders, os.path.join(report_path, datetime.datetime.now().strftime('财务需求_%Y-%m-%d.xls'))) 
+    xbr.report_of_sales(delivered_orders, os.path.join(report_path, datetime.datetime.now().strftime('小布销售统计_%Y-%m-%d.xls')))
+    xbr.report_of_revenue(undelivered_orders, os.path.join(report_path, datetime.datetime.now().strftime('小区单次营收统计_%Y-%m-%d.xls')))
+
+    # 按小区分类生成相关的报表
+    undelivered_data = undelivered_orders[1:]
+    communities = set(data_parser.get_data_from_column_title(XiaoBuReport.TITLE_COMMUNITY_POINT, undelivered_data))
+    community_index = data_parser.index_of_key(XiaoBuReport.TITLE_COMMUNITY_POINT)
+
+    for community in communities:
+        community_data = filter(lambda d: d[community_index] == community, undelivered_data)
+        community_data.insert(0, data_parser.get_header())
+        xbr.report_order_details_by_community(community, community_data, '订单详情.csv')
+        full_path_of_fruit_printing = os.path.join(report_path, community, '打印信息.xls') 
+        xbr.report_printing_information(community_data,full_path_of_fruit_printing)
